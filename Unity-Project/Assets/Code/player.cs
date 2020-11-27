8b325b3bfbbe4f9e7790f6179c697f048cb2f80a
Brandon <BrandonLuna2021@u.northwestern.edu>
2020-11-27 12:34:06 -0600

    player movement
    Added player movement of plane, background for scene, and the ability to switch between discrete or continous movement styles.

---
+++ Unity-Project/Assets/Code/player.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class player : MonoBehaviour
+{
+
+    /// rigid body object for the player
+    public Rigidbody2D player_rb;
+
+    /// continuous vs. discrete movement flag
+    public bool isDiscrete = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isDiscrete){
+            if(Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D)){
+                player_rb.MovePosition(Vector2.right + player_rb.position);
+            }
+            else if(Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A)){
+                player_rb.MovePosition(Vector2.left + player_rb.position);
+            }
+        }else{
+            if(Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)){
+                player_rb.MovePosition(Vector2.right + player_rb.position);
+            }
+            else if(Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)){
+                player_rb.MovePosition(Vector2.left + player_rb.position);
+            }
+        }
+
+    }
+}
