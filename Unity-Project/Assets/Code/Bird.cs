941f46d50aa4b1bbd7d2076f1a8f2497e5510982
Lazar Gueorguiev <lazargueorguiev@gmail.com>
2020-12-05 15:44:10 -0600

    bird spawner

---
+++ Unity-Project/Assets/Code/Bird.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        BoundsCheck();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+            Physics2D.IgnoreCollision(collision.gameObject.GetComponent<BoxCollider2D>(), GetComponent<BoxCollider2D>());
+        if (collision.gameObject.tag == "Player")
+        {
+            // Decrease Player health
+            Die();
+        }
+    }
+
+    private void BoundsCheck()
+    {
+        if (transform.position.y < -5f)
+            Die();
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Unity-Project/Assets/Code/BirdSpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdSpawner : MonoBehaviour
+{
+    public GameObject birdPrefab;
+    private readonly float speed = 1f;
+    private float timer = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        birdPrefab = Resources.Load("Bird") as GameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= 1 * Time.deltaTime;
+        if (timer <= 0)
+        {
+            Spawn();
+            timer = 5f;
+        }
+    }
+
+    public void Spawn()
+    {
+        GameObject g = Instantiate(birdPrefab, new Vector2(Random.Range(-4f, 4f), 6f), transform.rotation * Quaternion.Euler(0f, 0f, 180f));
+        g.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -1 * speed);
+    }
+}
